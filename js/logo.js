d25971b9723981623aa770295c86788257740847
J. Ryan Kelley <ryan.kelley@espn.com>
2014-09-25 23:12:10 -0700

    rotate the dots, neat.

--- js/logo.js
+++ js/logo.js
@@ -4,27 +4,42 @@
 	Logo.stage = new Kinetic.Stage( { 
 		container: 'header',
 		width: window.innerWidth,
-		height: 60
+		height: 110
 	} );
 	Logo.layer = new Kinetic.Layer();
 
 	// Circles
-	var i = 1;
-	for(var i = 0; i < 15; i++) {		
-		for(var j = 0; j < 5; j++) {
+	Logo.dotGroupA = new Kinetic.Group({x: Logo.stage.getWidth() / 2, y:50, offset: {x:125, y:125}});
+	Logo.dotGroupB = new Kinetic.Group({x: Logo.stage.getWidth() / 2, y:50, offset: {x:125, y:125}});
+	for(var i = 0; i < 10; i++) {		
+		for(var j = 0; j < 10; j++) {
 			var circle = new Kinetic.Circle( {
 				radius: 10,
 				fill: Math.floor(Math.random() * 2) == 1 ? '#2F2669' : '#BF4799',
 				strokeWidth: 0,
-				opacity: .4,
-				x: (i * 25 + Math.random() * 10),
-				y: j * 16 + Math.random() * 5
+				opacity: .3 + Math.random() / 5,
+				x: i * 25 + Math.random() * 10,
+				y: j * 25 + Math.random() * 5
 			} );
-			Logo.layer.add(circle);
+			Math.floor(Math.random() * 2)  == 1 ? Logo.dotGroupA.add(circle) : Logo.dotGroupB.add(circle);
 		}
 	}
+	Logo.layer.add(Logo.dotGroupA);
+	Logo.layer.add(Logo.dotGroupB);
+	Logo.stage.add(Logo.layer);
 
-    Logo.stage.add(Logo.layer);
+
+	// ANIMATIONS
+	Logo.rotateDots = new Kinetic.Animation(
+		function(frame,x) {
+			var angularSpeed = 360 / 75;
+			var angleDiff = frame.timeDiff * angularSpeed / 1000;
+			Logo.dotGroupA.rotate(angleDiff);
+			Logo.dotGroupB.rotate(-1 * angleDiff);
+		}, Logo.layer
+	);
+	Logo.rotateDots.start();
+
 
 
 })();
