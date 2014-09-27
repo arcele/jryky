133d0ce209a92e4ba3b87134d55b8181324a9d73
J. Ryan Kelley <ryan.kelley@espn.com>
2014-09-26 22:08:05 -0700

    better speed up/slow down management

--- js/logo.js
+++ js/logo.js
@@ -86,14 +86,15 @@
 	);
 
 	// EVENTS
+	Logo.angularSpeedMultiplier = 5;
 	Logo.title.on( 'mouseover click', function() {
-		Logo.angularSpeed = 360 / 30;
+		Logo.angularSpeed *= Logo.angularSpeedMultiplier;
 		Logo.compressTitleFont.stop();
 		Logo.expandTitleFont.start();
 		Logo.title.fill('#eadaea');
 	} );
 	Logo.title.on( 'mouseout', function() {
-		Logo.angularSpeed = 360 / 75;
+		Logo.angularSpeed /= Logo.angularSpeedMultiplier;
 		Logo.expandTitleFont.stop();
 		Logo.compressTitleFont.start();
 		Logo.title.fill('#aeaeae');
