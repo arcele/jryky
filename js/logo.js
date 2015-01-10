arcele/jryky

commit e89036b4551bd5f059495fe06b67240175a140d5

Moving logo into self executing function, true encapsulated singleton

js/logo.js
@@ -1,115 +1,122 @@
+var Logo;
 (function() {
+	var instance;
 
-	Logo = {};
+	Logo = function Logo() {
+		if(instance) {
+			return instance;
+		}
+		instance = this;
 
-	Logo.config = {
-		LOGO_COLOR: "#CFCFCF",
-		ZOOMED_LOGO_COLOR: "#EFEFFF",
-		LOGO_TEXT_FONT_SIZE: 30,
-		LOGO_TEXT_MAX_SIZE: 60,
-		LOGO_TEXT_OPACITY: .8,
-		DOT_ANGULAR_SPEED_MULTIPLIER: 10,
-		DOT_ROTATION_SPEED: 360 / 150000,
-		DOT_PRIMARY_COLOR: "#2F2669",
-		DOT_SECONDARY_COLOR: "#BF4799"
+		this.config = {
+			LOGO_COLOR: "#CFCFCF",
+			ZOOMED_LOGO_COLOR: "#EFEFFF",
+			LOGO_TEXT_FONT_SIZE: 30,
+			LOGO_TEXT_MAX_SIZE: 60,
+			LOGO_TEXT_OPACITY: .8,
+			DOT_ANGULAR_SPEED_MULTIPLIER: 10,
+			DOT_ROTATION_SPEED: 360 / 150000,
+			DOT_PRIMARY_COLOR: "#2F2669",
+			DOT_SECONDARY_COLOR: "#BF4799"
+		};
+
+		this.stage = new Kinetic.Stage( { 
+			container: 'header',
+			width: window.innerWidth,
+			height: 160
+		} );
+		this.layer = new Kinetic.Layer();
+
+		// Circles
+		this.dotGroupA = new Kinetic.Group( {
+			x: this.stage.getWidth() / 2, 
+			y:50, 
+			offset: {x:350, y:350}
+		} );
+		this.dotGroupB = new Kinetic.Group( {
+			x: this.stage.getWidth() / 2, 
+			y:50, 
+			offset: {x:350, y:350}
+		} );
+
+		for(var i = 0; i < 15; i++) {		
+			for(var j = 0; j < 15; j++) {
+				var circle = new Kinetic.Circle( {
+					radius: 10 + Math.random() * 6,
+					fill: Math.floor(Math.random() * 2) == 1 ? this.config.DOT_PRIMARY_COLOR : this.config.DOT_SECONDARY_COLOR,
+					strokeWidth: 0,
+					opacity: .3 + Math.random() / 5,
+					x: i * 50 + Math.random() * 15,
+					y: j * 50 + Math.random() * 15
+				} );
+				Math.floor(Math.random() * 2)  == 1 ? this.dotGroupA.add(circle) : this.dotGroupB.add(circle);
+			}
+		}
+		this.layer.add(this.dotGroupA);
+		this.layer.add(this.dotGroupB);
+		this.stage.add(this.layer);
+
+		this.dotGroupA.setRotation(Math.random() * 360);
+		this.dotGroupB.setRotation(Math.random() * 360);
+		this.dotGroupA.draw();
+		this.dotGroupB.draw();
+
+		this.title = new Kinetic.Text( {
+			x: this.stage.getWidth() / 2 - 85,
+			y: 30,
+			text: 'j. ryan kelley',
+			fontSize: this.config.LOGO_TEXT_FONT_SIZE,
+			fontFamily: 'Calibri',
+			fill: this.config.LOGO_COLOR,
+			opacity: this.config.LOGO_TEXT_OPACITY
+		} );
+		this.layer.add(this.title);
+		
+		// ANIMATIONS
+		this.angularSpeed = this.config.DOT_ROTATION_SPEED;
+		this.rotateDots = new Kinetic.Animation(
+			function(frame) {
+				var angleDiff = frame.timeDiff * this.angularSpeed;
+				this.dotGroupA.rotate(.3 * angleDiff);
+				this.dotGroupB.rotate(angleDiff);
+			}.bind(this), this.layer
+		);
+		this.rotateDots.start();
+
+		this.expandTitleFont = new Kinetic.Animation(
+			function(frame) {
+				if(this.title.getFontSize() >= this.config.LOGO_TEXT_MAX_SIZE) {
+					return false;
+					this.stop();
+				}
+				this.title.fontSize(this.title.getFontSize() + 3);
+				this.title.setX(this.title.getX() - 5);
+			}
+		);
+		this.compressTitleFont = new Kinetic.Animation(
+			function(frame) {
+				if(this.title.getFontSize() <= 35) {
+					return false;
+					this.stop();
+				}
+				this.title.fontSize(this.title.getFontSize() - 6);
+				this.title.setX(this.title.getX() + 10)
+			}
+		);
+
+		// EVENTS
+		this.title.on( 'mouseover click', function() {
+			this.angularSpeed /= this.config.DOT_ANGULAR_SPEED_MULTIPLIER;
+			this.compressTitleFont.stop();
+			this.expandTitleFont.start();
+			this.title.fill(this.config.ZOOMED_LOGO_COLOR);
+		} );
+		this.title.on( 'mouseout', function() {
+			this.angularSpeed *= this.config.DOT_ANGULAR_SPEED_MULTIPLIER;
+			this.expandTitleFont.stop();
+			this.compressTitleFont.start();
+			this.title.fill(this.config.LOGO_COLOR);
+		});
 	};
-
-	Logo.stage = new Kinetic.Stage( { 
-		container: 'header',
-		width: window.innerWidth,
-		height: 160
-	} );
-	Logo.layer = new Kinetic.Layer();
-
-	// Circles
-	Logo.dotGroupA = new Kinetic.Group( {
-		x: Logo.stage.getWidth() / 2, 
-		y:50, 
-		offset: {x:350, y:350}
-	} );
-	Logo.dotGroupB = new Kinetic.Group( {
-		x: Logo.stage.getWidth() / 2, 
-		y:50, 
-		offset: {x:350, y:350}
-	} );
-
-	for(var i = 0; i < 15; i++) {		
-		for(var j = 0; j < 15; j++) {
-			var circle = new Kinetic.Circle( {
-				radius: 10 + Math.random() * 6,
-				fill: Math.floor(Math.random() * 2) == 1 ? Logo.config.DOT_PRIMARY_COLOR : Logo.config.DOT_SECONDARY_COLOR,
-				strokeWidth: 0,
-				opacity: .3 + Math.random() / 5,
-				x: i * 50 + Math.random() * 15,
-				y: j * 50 + Math.random() * 15
-			} );
-			Math.floor(Math.random() * 2)  == 1 ? Logo.dotGroupA.add(circle) : Logo.dotGroupB.add(circle);
-		}
-	}
-	Logo.layer.add(Logo.dotGroupA);
-	Logo.layer.add(Logo.dotGroupB);
-	Logo.stage.add(Logo.layer);
-
-	Logo.dotGroupA.setRotation(Math.random() * 360);
-	Logo.dotGroupB.setRotation(Math.random() * 360);
-	Logo.dotGroupA.draw();
-	Logo.dotGroupB.draw();
-
-	Logo.title = new Kinetic.Text( {
-		x: Logo.stage.getWidth() / 2 - 85,
-		y: 30,
-		text: 'j. ryan kelley',
-		fontSize: Logo.config.LOGO_TEXT_FONT_SIZE,
-		fontFamily: 'Calibri',
-		fill: Logo.config.LOGO_COLOR,
-		opacity: Logo.config.LOGO_TEXT_OPACITY
-	} );
-	Logo.layer.add(Logo.title);
-	
-	// ANIMATIONS
-	Logo.angularSpeed = Logo.config.DOT_ROTATION_SPEED;
-	Logo.rotateDots = new Kinetic.Animation(
-		function(frame) {
-			var angleDiff = frame.timeDiff * Logo.angularSpeed;
-			Logo.dotGroupA.rotate(.3 * angleDiff);
-			Logo.dotGroupB.rotate(angleDiff);
-		}, Logo.layer
-	);
-	Logo.rotateDots.start();
-
-	Logo.expandTitleFont = new Kinetic.Animation(
-		function(frame) {
-			if(Logo.title.getFontSize() >= Logo.config.LOGO_TEXT_MAX_SIZE) {
-				return false;
-				this.stop();
-			}
-			Logo.title.fontSize(Logo.title.getFontSize() + 3);
-			Logo.title.setX(Logo.title.getX() - 5);
-		}
-	);
-	Logo.compressTitleFont = new Kinetic.Animation(
-		function(frame) {
-			if(Logo.title.getFontSize() <= 35) {
-				return false;
-				this.stop();
-			}
-			Logo.title.fontSize(Logo.title.getFontSize() - 6);
-			Logo.title.setX(Logo.title.getX() + 10)
-		}
-	);
-
-	// EVENTS
-	Logo.title.on( 'mouseover click', function() {
-		Logo.angularSpeed /= Logo.config.DOT_ANGULAR_SPEED_MULTIPLIER;
-		Logo.compressTitleFont.stop();
-		Logo.expandTitleFont.start();
-		Logo.title.fill(Logo.config.ZOOMED_LOGO_COLOR);
-	} );
-	Logo.title.on( 'mouseout', function() {
-		Logo.angularSpeed *= Logo.config.DOT_ANGULAR_SPEED_MULTIPLIER;
-		Logo.expandTitleFont.stop();
-		Logo.compressTitleFont.start();
-		Logo.title.fill(Logo.config.LOGO_COLOR);
-	})
 
 })();

index.php
@@ -6,7 +6,8 @@
     <meta name="keywords" content="James Kelley, James Ryan Kelley, Ryan Kelley, web development, javascript, HTML5, rails, coconut water" />
     <script src="js/jquery-2.0.2.min.js"></script>
     <script src="js/kinetic.js"></script>
-    <script src="js/jryky.js?v=2"></script>
+	<script src="js/jryky.js?v=2"></script>
+	<script src="js/logo.js"></script>
     <link href="css/jryky.css?v=2" rel="stylesheet" type="text/css" />
   </head>
   <body>

js/jryky.js
@@ -1,6 +1,11 @@
 $(document).ready(
+	function() {
+		logo = new Logo();
+	}
+	/*
     function() {
 	$.getScript('js/logo.js');
     }
+    */
 );
 

commit 85f7685975a0da197bb1d1c61af95a51bbdd5a2a

fixing some scope issues caused by singleton changes in e89036b

js/logo.js
@@ -91,7 +91,7 @@ var Logo;
 				}
 				this.title.fontSize(this.title.getFontSize() + 3);
 				this.title.setX(this.title.getX() - 5);
-			}
+			}.bind(this)
 		);
 		this.compressTitleFont = new Kinetic.Animation(
 			function(frame) {
@@ -101,7 +101,7 @@ var Logo;
 				}
 				this.title.fontSize(this.title.getFontSize() - 6);
 				this.title.setX(this.title.getX() + 10)
-			}
+			}.bind(this)
 		);
 
 		// EVENTS
@@ -110,13 +110,13 @@ var Logo;
 			this.compressTitleFont.stop();
 			this.expandTitleFont.start();
 			this.title.fill(this.config.ZOOMED_LOGO_COLOR);
-		} );
+		}.bind(this));
 		this.title.on( 'mouseout', function() {
 			this.angularSpeed *= this.config.DOT_ANGULAR_SPEED_MULTIPLIER;
 			this.expandTitleFont.stop();
 			this.compressTitleFont.start();
 			this.title.fill(this.config.LOGO_COLOR);
-		});
+		}.bind(this));
 	};
 
 })();