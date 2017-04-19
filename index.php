1a290354917feaa7c3ec66265711f143771e6227
arcele <arcele@gmail.com>
2017-04-19 14:24:37 -0600

    removing some accounts due to privacy concerns

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <script src="js/jryky.js?v=2"></script>
     <script src="js/logo.js"></script>
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Droid+Sans">
-    <link href="css/jryky.css?v=2" rel="stylesheet" type="text/css" />
+    <link href="css/jryky.css?v=5" rel="stylesheet" type="text/css" />
   </head>
   <body>
 <?php  	
@@ -20,8 +20,6 @@
     <div id="subnav">
       <div id="links">
         <a href="http://twitter.com/arcele" target="_twit"><span class="logo"><img src="img/social/twitter.png"></span><span class="text">twitter</span></a>
-        <a href="http://facebook.com/jryankelley" target="_face"><span class="logo"><img src="img/social/facebook.png"></span><span class="text">facebook</span></a>
-        <a href="http://instagram.com/jryankelley" target="_gram"><span class="logo"><img src="img/social/instagram.png"></span><span class="text">instagram</span></a>
         <a href="http://github.com/arcele" target="_git"><span class="logo"><img src="img/social/github.png"></span><span class="text">github</span></a>
         <a href="http://linkedin.com/in/jryankelley" target="_linked"><span class="logo"><img src="img/social/linkedin.png" /></span><span class="text">linkedin</span></a>
         <a href="mailto:ryan@jryky.com"><span class="logo"><img src="img/social/email.png" /></span><span class="text">email</span></a>
@@ -30,19 +28,11 @@
     </div>
     <div id="content">
       
-      <section class="blurb"><div class="profile_pic">&nbsp;</div>Hey, I'm Ryan.  I like to build things with ones and zeros.  Okay, I'm not exactly doing assembly programming, but I enjoy building easy to understand user interfaces on top of scaleable back end solutions.  I've been programming since I was 11, QBasic was my jam, and I like to play with all sorts of languages and technologies.  Most of my professional experience has been on the web development side of things, a ton of front end work and a nice chunk of full stack development.  I work with HTML, CSS, and Javascript a lot.  Like, a whole lot.  Recently I've been playing around a lot with <a href="https://nodejs.org" target="_out">Node.js</a> which allows me to do all of my dev work in javascript, something that might have sounded ridiculous if you told me that I'd be doing it ten years ago.  Over the years I've worked with tons of different languages like PHP, Ruby On Rails, XUL, Python, tea, and lots of cool javascript frameworks from prototype to jQuery, and my favorite HTML canvas manimpulation library, KineticJS.  I've also written tons of vanilla javascript.</section>
-      
-      <section class="blurb">Sometimes, I'll just throw something together,<a href="http://retirethe5.com" target="_out">like a random fantasy team logo generator using the HTML canvas</a>.  Sometimes, I miss playing bubble hockey with my buddies so I throw together <a href="http://nwbhf.com/" target="_out">a canvas bubble hockey game</a> that needs some love on the collision detection side.  Sometimes, I build something simple that <a href="http://sevensixtwo.com/" target="_out">serves legitimately no purpose</a>, because, why not?  Playing around and experiementing with different technologies and different concepts keeps my mind fresh and keeps the work exciting.</section>
-      
-      <section class="blurb">I like to mess around with graphics and logo design as well.  I've done a fair amount of web development on the side that required me to also mock up designs in photoshop, but, I really like to play around with logos.  My love of logo design comes from the fact that I've always been intrigued by sports logos and uniforms, one of my favorite sites on the subject is, <a href="http://www.uni-watch.com/" target="_out">UniWatch</a>. </section>
-      
-      <section class="blurb">When I'm not sitting at my computer, I mostly listen to music and like to get out and explore.  I'm an <a href="https://www.athlinks.com/athletes/190914181" target="_out">avid runner</a> who has been trying for years to retire from the half marathon game, unsuccessfully.  Every time I think I'm out, someone invites me to run one with them.  I also like to do <a href="https://www.instagram.com/hookgrip/" target="_out">olympic weight lifting</a>, although, I'm far from an olympian.  I've also got a <a href="http://burtonthedog.isbetterthanyou.com/" target="_out">Greater Swiss Mountain Dog</a> who I enjoy both lounging on the couch and hiking around the wilderness with.  When it comes to music, I've got a pretty wide area of interest, but my favorite is the <a href="http://rhymesayers.com/" target="_out">Rap Music that comes out of Minneapolis, MN</a>.  I know, people always think I'm crazy, if you've never heard any of it, check out <a href="https://www.youtube.com/watch?v=VFt1xMPnkVg" target="_out">Atmosphere</a>, they are my absolute favorite.</section>
-      
-      <section class="blurb">Anyway, thanks for coming by or whatever, if you want to bug me about anything from hating my site to wanting to know more about Minnesota Rap music <a href="mailto:ryan@jryky.com">hit me up via e-mail</a>.</section>
+      <section class="blurb"><div class="profile_pic">&nbsp;</div>Hey, I'm Ryan.</section> <!-- ' -->
 	
     </div>
     <div id="footer">
-      &copy;2015 <a href="mailto:ryan@jryky.com">j. ryan kelley </a> | <a href="http://twitter.com/arcele" target="_out">twitter</a> | <a href="http://facebook.com/jryankelley" target="_out">facebook</a> | <a href="http://instagram.com/jryankelley">instagram</a> | <a href="http://github.com/arcele" target="_out">GitHub</a> | <a href="http://linkedin.com/in/jryankelley" target="_out">LinkedIn</a>
+      &copy;2015 <a href="mailto:ryan@jryky.com">j. ryan kelley </a> | <a href="http://twitter.com/arcele" target="_out">twitter</a> | <a href="http://github.com/arcele" target="_out">GitHub</a> | <a href="http://linkedin.com/in/jryankelley" target="_out">LinkedIn</a>
     </div>
   </body>
 </html>
